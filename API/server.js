7c06929c23c12db7539a04e4387b6d82837d37b9
Krishanrx8 <krishanrx8@gmail.com>
2023-03-11 02:28:06 +0530

    Server changes done

--- API/server.js
+++ API/server.js
@@ -36,11 +36,12 @@ app.get('/user', (req, res) => {
 //Get One user
 app.get('/user/:id', (req, res) => {
     var id = req.params.id;
-    console.log("request received for get One user");
+    console.log("request received for get One user - " + id);
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("LoanSystem");
-        dbo.collection("user").find({ NIC: id }).toArray(function (err, result) {
+        var o_id = new mongo.ObjectID(id);
+        dbo.collection("user").find({ _id: o_id }).toArray(function (err, result) {
             if (err) throw err;
             res.send(result);
             db.close();
@@ -58,9 +59,9 @@ app.post('/user', (req, res) => {
     memberObj.DOB = req.body.DOB;
     memberObj.Email = req.body.email;
     memberObj.Password = req.body.password;
-    memberObj.LoanAmount = 15000;
-    memberObj.UsedAmount = 0;
-    memberObj.InstalmentPlan = 6;
+    memberObj.LoanBalance = 15000;
+    memberObj.UsedAmount = 15000;
+    memberObj.InstalmentPlan = 3;
 
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
@@ -75,24 +76,33 @@ app.post('/user', (req, res) => {
 });
 
 //Update user
-app.put('/user', (req, res) => {
-
+app.put('/user/:id', (req, res) => {
+    var id = req.params.id;
     var memberObj = new Object();
 
-    memberObj.cusID = req.body.cusID;
-    memberObj.FullName = req.body.FullName;
+    memberObj.FullName = req.body.name;
+    memberObj.Address = req.body.address;
     memberObj.DOB = req.body.DOB;
-    memberObj.LoanAmount = req.body.LoanAmount;
-    memberObj.UsedAmount = req.body.UsedAmount;
-    memberObj.InstalmentPlan = req.body.InstalmentPlan;
+    memberObj.Email = req.body.email;
+    memberObj.LoanBalance = req.body.loanU;;
+    memberObj.UsedAmount = req.body.UsedAmountU;
 
+    console.log(memberObj);
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("LoanSystem");
 
-        var myquery = { NIC: memberObj.NIC };
-        var newvalues = { $set: { Status: memberObj.Status } };
-
+        var myquery = { _id: mongo.ObjectID(id) };
+        var newvalues = {
+            $set: {
+                FullName: memberObj.FullName,
+                Address: memberObj.Address,
+                DOB: memberObj.DOB,
+                Email: memberObj.Email,
+                LoanBalance: memberObj.LoanBalance,
+                UsedAmount: memberObj.UsedAmount
+            }
+        };
         dbo.collection("user").updateOne(myquery, newvalues, function (err1, res1) {
             if (err1) throw err1;
             res.send(true);
@@ -101,6 +111,170 @@ app.put('/user', (req, res) => {
     });
 });
 
+//Update user by user
+app.put('/userByUser/:id', (req, res) => {
+    var id = req.params.id;
+    var memberObj = new Object();
+
+    memberObj.FullName = req.body.name;
+    memberObj.Address = req.body.address;
+    memberObj.DOB = req.body.DOB;
+    memberObj.Email = req.body.email;
+    memberObj.Password = req.body.password;
+
+    console.log(memberObj);
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("LoanSystem");
+
+        var myquery = { _id: mongo.ObjectID(id) };
+        var newvalues = {
+            $set: {
+                FullName: memberObj.FullName,
+                Address: memberObj.Address,
+                DOB: memberObj.DOB,
+                Email: memberObj.Email,
+                Password: memberObj.Password
+            }
+        };
+        dbo.collection("user").updateOne(myquery, newvalues, function (err1, res1) {
+            if (err1) throw err1;
+            res.send(true);
+            db.close();
+        });
+    });
+});
+
+
+//delete User
+app.delete('/user/:id', function (req, res) {
+    var id = req.params.id;
+    console.log("deleting" + id);
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("LoanSystem");
+        var myquery = { _id: mongo.ObjectID(id) };
+        dbo.collection("user").deleteOne(myquery, function (err1, result) {
+            if (err) throw err;
+            res.send(result);
+            db.close();
+        });
+    });
+});
+
+//Get all Payment Request
+app.get('/payment', (req, res) => {
+    console.log("request received for get all Payment Request");
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("LoanSystem");
+        dbo.collection("payment").find({}).toArray(function (err, result) {
+            if (err) throw err;
+            res.send(result);
+            db.close();
+        });
+    });
+});
+
+//Get One Payment Request
+app.get('/payment/:id', (req, res) => {
+    var id = req.params.id;
+    console.log("request received for get One Payment Request - " + id);
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("LoanSystem");
+        var o_id = new mongo.ObjectID(id);
+        dbo.collection("payment").find({ _id: o_id }).toArray(function (err, result) {
+            if (err) throw err;
+            res.send(result);
+            db.close();
+        });
+    });
+});
+
+//Create Payment Request
+app.post('/payment', (req, res) => {
+    console.log("Create Payment Request started");
+    var memberObj = new Object();
+
+    memberObj.userID = req.body.userID;
+    memberObj.Date = req.body.Date;
+    memberObj.BankName = req.body.BankName;
+    memberObj.BranchName = req.body.BranchName;
+    memberObj.BranchCode = req.body.BranchCode;
+    memberObj.Amount = req.body.Amount;
+    memberObj.Status = "Pending";
+    memberObj.Reason = "";
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("LoanSystem");
+        dbo.collection("payment").insertOne(memberObj, function (err1, res1) {
+            if (err1) throw err1;
+            res.send("Success");
+            db.close();
+        });
+    });
+
+});
+
+//Accept the payment
+app.put('/acceptPayment/:id', (req, res) => {
+    var id = req.params.id;
+    var memberObj = new Object();
+
+    memberObj.NewAmount = req.body.NewAmount;
+    memberObj.userID = req.body.userID;
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("LoanSystem");
+
+        var myquery = { _id: mongo.ObjectID(memberObj.userID) };
+        var newvalues = { $set: { LoanBalance: memberObj.NewAmount } };
+        dbo.collection("user").updateOne(myquery, newvalues, function (err1, res1) {
+            if (err1) throw err1;
+
+            db.close();
+        });
+    });
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("LoanSystem");
+
+        var myquery = { _id: mongo.ObjectID(id) };
+        var newvalues = { $set: { Status: "Accepted" } };
+        dbo.collection("payment").updateOne(myquery, newvalues, function (err1, res1) {
+            if (err1) throw err1;
+            res.send(true);
+            db.close();
+        });
+    });
+});
+
+//Reject the payment
+app.put('/rejectPayment/:id', (req, res) => {
+    var id = req.params.id;
+    var memberObj = new Object();
+
+    memberObj.Reason = req.body.Reason;
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("LoanSystem");
+
+        var myquery = { _id: mongo.ObjectID(id) };
+        var newvalues = { $set: { Status: "Rejected", Reason: memberObj.Reason } };
+        dbo.collection("payment").updateOne(myquery, newvalues, function (err1, res1) {
+            if (err1) throw err1;
+            res.send(true);
+            db.close();
+        });
+    });
+
+});
 
 
 
--- js/main.js
+++ js/main.js
@@ -26,7 +26,109 @@ function viewLogin() {
 }
 
 
+function enableEditing() {
+	document.getElementById("saveBtn").style.display = "inline";
+	document.getElementById("cancelBtn").style.display = "inline";
+	document.getElementById("enableEditingBtn").style.display = "none";
 
+	document.getElementById("name").removeAttribute('disabled');
+	document.getElementById("address").removeAttribute('disabled');
+	document.getElementById("DOB").removeAttribute('disabled');
+	document.getElementById("email").removeAttribute('disabled');
+	document.getElementById("pw").removeAttribute('disabled');
+}
+
+function calcelEditing() {
+	window.location.reload();
+}
+
+function updateData() {
+	var obj = new Object();
+	obj.name = document.getElementById("name").value;
+	obj.address = document.getElementById("address").value;
+	obj.DOB = document.getElementById("DOB").value;
+	obj.email = document.getElementById("email").value;
+	obj.password = document.getElementById("pw").value;
+
+	$.ajax({
+		url: 'http://localhost:5000/userByUser/' + localStorage.getItem("loggedInUserLoanManagementSystem"),
+		type: 'PUT',
+		data: obj,
+		success: function (result) {
+			window.location.reload();
+		}
+	});
+}
+
+function loadUserData() {
+	var loggedInUserID = localStorage.getItem("loggedInUserLoanManagementSystem");
+	document.getElementById("userID").value = loggedInUserID;
+
+	$.ajax({
+		url: 'http://localhost:5000/user/' + loggedInUserID,
+		type: 'GET',
+		success: function (result) {
+			console.log(result);
+			document.getElementById("name").value = result[0].FullName;
+			document.getElementById("address").value = result[0].Address;
+			document.getElementById("DOB").value = result[0].DOB;
+			document.getElementById("email").value = result[0].Email;
+			document.getElementById("pw").value = result[0].Password;
+			document.getElementById("UsedAmount").innerHTML = result[0].UsedAmount + " LKR";
+			document.getElementById("loanBalance").innerHTML = result[0].LoanBalance + " LKR";
+		}
+	});
+
+	$.ajax({
+		url: 'http://localhost:5000/payment',
+		type: 'GET',
+		success: function (result) {
+
+			for (let i = 0; i < result.length; i++) {
+				if (result[i].userID == loggedInUserID) {
+					if (result[i].Status == "Pending") {
+						document.getElementById("userTblData").innerHTML +=
+							"<tr>" +
+							"  <td>" + result[i].Date + "</td>" +
+							"  <td>" + result[i].BankName + "</td>" +
+							"  <td>" + result[i].BranchName + "</td>" +
+							"  <td>" + result[i].BranchCode + "</td>" +
+							"  <td>" + result[i].Amount + "</td>" +
+							"  <td><span class=\"badge bg-warning\">pending</span></td>" +
+							"</tr>";
+					}
+					if (result[i].Status == "Accepted") {
+						document.getElementById("userTblData").innerHTML +=
+							"<tr>" +
+							"  <td>" + result[i].Date + "</td>" +
+							"  <td>" + result[i].BankName + "</td>" +
+							"  <td>" + result[i].BranchName + "</td>" +
+							"  <td>" + result[i].BranchCode + "</td>" +
+							"  <td>" + result[i].Amount + "</td>" +
+							"  <td><span class=\"badge bg-success\">accepted</span></td>" +
+							"</tr>";
+					}
+					if (result[i].Status == "Rejected") {
+						document.getElementById("userTblData").innerHTML +=
+							"<tr>" +
+							"  <td>" + result[i].Date + "</td>" +
+							"  <td>" + result[i].BankName + "</td>" +
+							"  <td>" + result[i].BranchName + "</td>" +
+							"  <td>" + result[i].BranchCode + "</td>" +
+							"  <td>" + result[i].Amount + "</td>" +
+							"  <td><span class=\"btn badge bg-danger\" onclick=\"showReason('" + result[i].Reason + "')\">rejected</span></td>" +
+							"</tr>";
+					}
+				}
+			}
+		}
+	});
+}
+
+function showReason(reason) {
+	$('#reasonDataModal').modal('show');
+	document.getElementById("msg").innerHTML = reason;
+}
 
 function login() {
 	var isFound = false;
@@ -39,8 +141,10 @@ function login() {
 			type: 'GET',
 			success: function (result) {
 				for (let i = 0; i < result.length; i++) {
-					if (result[i].username == document.getElementById("username").value && result[i].password == document.getElementById("pw1").value) {
+					if (result[i].Email == document.getElementById("username").value && result[i].Password == document.getElementById("pw1").value) {
 						isFound = true;
+						localStorage.setItem("loggedInUserLoanManagementSystem", result[i]._id);
+
 						window.location.href = "./user.html";
 					}
 				}
@@ -64,7 +168,7 @@ function loadData() {
 					"<td>" + result[i]._id + "</td>" +
 					"<td>" + result[i].FullName + "</td>" +
 					"<td>" + result[i].DOB + " </td>" +
-					"<td>" + result[i].LoanAmount + "</td>" +
+					"<td>" + result[i].LoanBalance + "</td>" +
 					"<td>" + result[i].UsedAmount + " </td>" +
 					"<td>" + result[i].InstalmentPlan + " </td>" +
 					"</tr>";
@@ -74,7 +178,116 @@ function loadData() {
 	});
 }
 
+function loadDataRequests() {
+	$.ajax({
+		url: 'http://localhost:5000/payment',
+		type: 'GET',
+		success: function (result) {
+			console.log(result);
+			for (let i = 0; i < result.length; i++) {
+				if (result[i].Status == "Pending") {
+					document.getElementById("allData").innerHTML +=
+						"<div class=\"shadow p-3 mb-5 bg-body rounded w-75 mx-auto\">" +
+						"<div class=\"row\">" +
+						"	<div class=\"col\">" +
+						"		<h6>Request ID - " + result[i]._id + "</h6>" +
+						"	</div>" +
+						"	<div class=\"col text-end\">" +
+						"		<h6>User ID - " + result[i].userID + "</h6>" +
+						"	</div>" +
+						"</div>" +
+						"<table class=\"table table-bordered my-4\">" +
+						"  <thead>" +
+						"	<tr>" +
+						"	  <th scope=\"col\" style=\"width : 20%;\">Payment Date</th>" +
+						"	  <th scope=\"col\" style=\"width : 40%;\">Bank Name</th>" +
+						"	  <th scope=\"col\" style=\"width : 15%;\">Brach Name</th>" +
+						"	  <th scope=\"col\" style=\"width : 10%;\">Branch Code</th>" +
+						"	  <th scope=\"col\" style=\"width : 15%;\">Amount</th>" +
+						"	</tr>" +
+						" </thead>" +
+						"  <tbody>" +
+						"	<tr>" +
+						"	  <td>" + result[i].Date + "</td>" +
+						"	  <td>" + result[i].BankName + "</td>" +
+						"	  <td>" + result[i].BranchName + "</td>" +
+						"	  <td>" + result[i].BranchCode + "</td>" +
+						"	  <td>" + result[i].Amount + "</td>" +
+						"	</tr>" +
+						" </tbody>" +
+						"</table>" +
+						"<button type=\"button\" class=\"btn btn-success\" onclick=\"acceptRequest('" + result[i]._id + "' , '" + result[i].userID + "' , '" + result[i].Amount + "')\">ACCEPT</button>" +
+						"<button type=\"button\" class=\"btn btn-danger mx-2\" onclick=\"rejectRequest('" + result[i]._id + "')\">REJECT</button>" +
+						"</div>";
+				}
+			}
+		}
+	});
+}
+
+function acceptRequest(recordID, userID, Amount) {
+	var newAmount = 0;
+	$.ajax({
+		url: 'http://localhost:5000/user/' + userID,
+		type: 'GET',
+		success: function (result) {
+			newAmount = Number(result[0].LoanBalance) - Number(Amount);
+
+			var obj = new Object();
+			obj.NewAmount = newAmount;
+			obj.userID = userID;
+
+
+			$.ajax({
+				url: 'http://localhost:5000/acceptPayment/' + recordID,
+				type: 'PUT',
+				data: obj,
+				success: function (result) {
+					window.location.reload();
+				}
+			});
+
+		}
+	});
+
+}
+
+var rejectID = "";
+function rejectRequest(recordID) {
+	rejectID = recordID;
+	$('#reasonModal').modal('show');
+}
+
+function rejectConfimed() {
+	var obj = new Object();
+	obj.Reason = document.getElementById("reason").value;
+
+	$.ajax({
+		url: 'http://localhost:5000/rejectPayment/' + rejectID,
+		type: 'PUT',
+		data: obj,
+		success: function (result) {
+			window.location.reload();
+		}
+	});
+}
+
+
 function selectRow(data) {
+	document.getElementById("cusID").innerHTML = data;
+	$.ajax({
+		url: 'http://localhost:5000/user/' + data,
+		type: 'GET',
+		success: function (result) {
+			console.log(result);
+			document.getElementById("nameU").value = result[0].FullName;
+			document.getElementById("addressU").value = result[0].Address;
+			document.getElementById("DOBU").value = result[0].DOB;
+			document.getElementById("emailU").value = result[0].Email;
+			document.getElementById("loanU").value = result[0].LoanBalance;
+			document.getElementById("UsedAmountU").value = result[0].UsedAmount;
+		}
+	});
 	console.log(data);
 	$('#userModal').modal('show');
 }
@@ -94,7 +307,7 @@ function createUSer() {
 			alert("Please Enter Valid Email. Its Can not be Empty");
 			break;
 		case document.getElementById("pw2").value:
-			alert("Please Enter Valid password. Its Can not be Empty - " + document.getElementById("pw2").value);
+			alert("Please Enter Valid password. Its Can not be Empty");
 			break;
 		default:
 			var obj = new Object();
@@ -116,6 +329,7 @@ function createUSer() {
 							success: function (result) {
 								for (let i = 0; i < result.length; i++) {
 									if (result[i].Email == document.getElementById("email").value && result[i].Password == document.getElementById("pw2").value) {
+										localStorage.setItem("loggedInUserLoanManagementSystem", result[i]._id);
 										window.location.href = "./user.html";
 									}
 								}
@@ -130,3 +344,70 @@ function createUSer() {
 	}
 }
 
+function submitPaymentRequest() {
+	switch ("") {
+		case document.getElementById("date").value:
+			alert("Please Enter Valid Date. Its Can not be Empty");
+			break;
+		case document.getElementById("bankName").value:
+			alert("Please Enter Valid Bank Name. Its Can not be Empty");
+			break;
+		case document.getElementById("branchName").value:
+			alert("Please Enter Valid Branch Name. Its Can not be Empty");
+			break;
+		case document.getElementById("branchCode").value:
+			alert("Please Enter Valid Branch Code. Its Can not be Empty");
+			break;
+		case document.getElementById("amount").value:
+			alert("Please Enter Valid Amount. Its Can not be Empty");
+			break;
+		default:
+			var obj = new Object();
+			obj.userID = document.getElementById("userID").value;
+			obj.Date = document.getElementById("date").value;
+			obj.BankName = document.getElementById("bankName").value;
+			obj.BranchName = document.getElementById("branchName").value;
+			obj.BranchCode = document.getElementById("branchCode").value;
+			obj.Amount = document.getElementById("amount").value;
+
+			$.ajax({
+				url: 'http://localhost:5000/payment',
+				type: 'POST',
+				data: obj,
+				success: function (result) {
+					window.location.reload();
+				}
+			});
+	}
+}
+
+function deleteUser() {
+	$.ajax({
+		url: 'http://localhost:5000/user/' + document.getElementById("cusID").innerHTML,
+		type: 'DELETE',
+		success: function (result) {
+			window.location.reload();
+		}
+	});
+}
+
+function updateUser() {
+	var obj = new Object();
+	obj.name = document.getElementById("nameU").value;
+	obj.address = document.getElementById("addressU").value;
+	obj.DOB = document.getElementById("DOBU").value;
+	obj.email = document.getElementById("emailU").value;
+	obj.loanU = document.getElementById("loanU").value;
+	obj.UsedAmountU = document.getElementById("UsedAmountU").value;
+
+	$.ajax({
+		url: 'http://localhost:5000/user/' + document.getElementById("cusID").innerHTML,
+		type: 'PUT',
+		data: obj,
+		success: function (result) {
+			window.location.reload();
+		}
+	});
+
+
+}
